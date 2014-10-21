c47e29e8cd99b8545de1f6558a49ca3b4b661dd2
Keishi Hattori <keishi.hattori@gmail.com>
2014-10-21 18:19:24 +0900

    Updated run_blink_benchmarks to put results in individual directories

--- run_blink_benchmarks.sh
+++ run_blink_benchmarks.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 while read test_name; do
-  $CHROMIUM_DIR/src/tools/perf/run_benchmark run --browser=release $test_name --output-format=json --output-dir ~/bench-master-oilpan --page-repeat=10
+  $CHROMIUM_DIR/src/tools/perf/run_benchmark run --browser=release $test_name --output-format=json --output-dir ~/bench-master-oilpan/$test_name --page-repeat=10
 done < benchmarks.txt
