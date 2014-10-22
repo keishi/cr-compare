c236b40e4547ce2691d3e1607526f27dc61f1f94
Keishi Hattori <keishi.hattori@gmail.com>
2014-10-22 11:23:03 +0900

    run_blink_benchmarks.sh can be run from any directory

--- run_blink_benchmarks.sh
+++ run_blink_benchmarks.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 while read test_name; do
-  $CHROMIUM_DIR/src/tools/perf/run_benchmark run --browser=release $test_name --output-format=json --output-dir ~/bench-master-oilpan/$test_name --page-repeat=10
-done < benchmarks.txt
+  $CHROMIUM_DIR/src/tools/perf/run_benchmark run --browser=release $test_name --output-format=json --output-dir $1/$test_name --page-repeat=10
+done < `dirname $0`/benchmarks.txt
